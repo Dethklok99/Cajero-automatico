9f4e287a5c5b8741d560b18011d2e6904eb58c66
Dethklok99 <77636853+Dethklok99@users.noreply.github.com>
2021-01-26 17:22:36 -0600

    Update Cajero automatico.cpp

--- Cajero automatico.cpp
+++ Cajero automatico.cpp
@@ -7,10 +7,11 @@
 using namespace std;
 
 int main(){
-	int usuario,user[3] = {300,301,302},PIN[3] = {1357,2468,9182},opc;
-	int saldo_inicial = 10000,i,s,pines,a;
-	float saldo = 0,extra,impri;
-	char retiro;
+	int usuario,user[3] = {300,301,302},PIN[3] = {1357,2468,9182},opc,a;
+	int saldo_inicial = 10000,i,s,pines;
+	double saldo = 0;
+	int retiro,impri;
+	char extra;
 	
 	cout<<"\tBienvenido al Cajero Automatico\n"<<endl;
 	
@@ -37,49 +38,52 @@ int main(){
 			break;
 		}
 	}
+		do{
+			conexion:
+			system ("cls");
+			cout<<"\tBienvenido a las opciones del cajero"<<endl;
+			cout<<"Elija una opcion:"<<endl;
+			cout<<"1.- Consultar saldo"<<endl;
+			cout<<"2.- Retirar efectivo"<<endl;
+			cout<<"3.- Cargar saldo"<<endl;
+			cout<<"4.- Imprimir saldo"<<endl;
+			cout<<"5.- Salir"<<endl;
+			cin>>opc;
 	
-	cout<<"\tBienvenido a las opciones del cajero"<<endl;
-	cout<<"Elija una opcion:"<<endl;
-	cout<<"1.- Consultar saldo"<<endl;
-	cout<<"2.- Retirar efectivo"<<endl;
-	cout<<"3.- Cargar saldo"<<endl;
-	cout<<"4.- Imprimir saldo"<<endl;
-	cout<<"5.- Salir"<<endl;
-	cin>>opc;
-	
-	switch(opc){
-		case 1:
-			cout<<"Su saldo es de: "<<saldo_inicial<<endl;
-			cin>>saldo_inicial;
-		case 2:
-			cout<<"Digite la cantidad de dinero que desea retirar: "<<retiro<<endl;break;
-			cin>>retiro;
+			switch(opc){
+			case 1:
+				cout<<"Su saldo es de: "<<saldo_inicial<<endl;
+				cin>>saldo_inicial;
+			case 2:
+				cout<<"Digite la cantidad de dinero que desea retirar: "<<endl;
+				cin>>retiro;
 			
+				if(retiro > saldo_inicial){
+					cout<<"No cuenta con saldo suficiente para realizar la transaccion"<<endl;
+				}
+				else{
+					saldo = saldo_inicial - retiro;
+					cout<<"Su nuevo saldo es de: "<<saldo<<endl;				
+				}	
+							
+			case 3: 
+				cout<<"Digite la cantidad de dinero que desea ingresar: "<<extra<<endl;
+				cin>>extra;
+				saldo = saldo_inicial + extra;
+				cout<<"Su nuevo saldo es de: "<<saldo;
 			
-			if(retiro > saldo_inicial){
-				cout<<"No cuenta con saldo suficiente para realizar la transaccion"<<endl;
+			case 4:
+				cout<<"Imprimiendo saldo: "<<impri;
+				cin>>impri;
+				impri = saldo_inicial;
+				cout<<"Su saldo impreso es de: "<<saldo;
+			
+			case 5: continue;	
+		
 			}
-			else{
-				saldo = saldo_inicial - retiro;
-				cout<<"Su nuevo saldo es de: "<<saldo<<endl;break;
-			}
-			
-		case 3: 
-			cout<<"Digite la cantidad de dinero que desea ingresar: "<<extra<<endl;
-			cin>>extra;
-			saldo = saldo_inicial + extra;
-			cout<<"Su nuevo saldo es de: "<<saldo;break;
-			
-		case 4:
-			cout<<"Imprimiendo saldo: "<<impri;
-			cin>>impri;
-			impri = saldo_inicial;
-			cout<<"Su saldo impreso es de: "<<saldo;break;
-			
-		case 5: break;
-	}	
+		}while(opc != 5);
 	
 	system("pause");
 	getch();
-	return 0;
+	return 0;	
 }
