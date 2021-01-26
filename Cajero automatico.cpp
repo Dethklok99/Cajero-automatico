9d4dd49750d9f9e4c7ba6bfdb1bce1a407e1b521
Dethklok99 <77636853+Dethklok99@users.noreply.github.com>
2021-01-26 14:12:38 -0600

    Update Cajero automatico.cpp

--- Cajero automatico.cpp
+++ Cajero automatico.cpp
@@ -1,32 +1,42 @@
 //Cajero automatico//
 
-
 #include<iostream>
+#include<conio.h>
+#include<stdlib.h>
 
 using namespace std;
 
 int main(){
-	int usuario1,ramon,PIN,opc;
-	int saldo_inicial = 10000;
-	float saldo = 0,retiro,extra,impri;
+	int usuario,user[3] = {300,301,302},PIN[3] = {1357,2468,9182},opc;
+	int saldo_inicial = 10000,i,s,pines,a;
+	float saldo = 0,extra,impri;
+	char retiro;
 	
 	cout<<"\tBienvenido al Cajero Automatico\n"<<endl;
 	
 	cout<<"Ingresa tu usuario: "<<endl;
-	cin>>usuario1;
-	usuario1 = ramon;
-	
-	if(usuario1 == ramon){
-		cout<<"El usuario es correcto"<<endl;
-	}
-	else{
-		cout;"Usuario no valido";
+	cin>>usuario;
+	for(i = 0; i < 3; i++){
+		if(usuario != user[i]){
+			continue;
+		}	
+		else{
+			a = i;
+			break;
+		}	
 	}
 	
 	cout<<"Ingresa tu PIN:"<<endl;
-	cin>>PIN;
-	PIN = 12345;
-	
+	cin>>pines;
+	for(i = 0; i < 3; i++){
+		if(pines != PIN[i]){
+			continue;
+		}
+		else{
+			s = i;
+			break;
+		}
+	}
 	
 	cout<<"\tBienvenido a las opciones del cajero"<<endl;
 	cout<<"Elija una opcion:"<<endl;
@@ -39,25 +49,23 @@ int main(){
 	
 	switch(opc){
 		case 1:
-			cout<<"Su saldo es de: "<<endl;
-			cin>>saldo;
-			saldo = saldo_inicial;
-			cout<<"Usted cuenta con la cantidad de: "<<saldo;break;
-			
+			cout<<"Su saldo es de: "<<saldo_inicial<<endl;
+			cin>>saldo_inicial;
 		case 2:
-			cout<<"Digite la cantidad de dinero que desea retirar: "<<retiro;
+			cout<<"Digite la cantidad de dinero que desea retirar: "<<retiro<<endl;break;
 			cin>>retiro;
 			
+			
 			if(retiro > saldo_inicial){
-				cout<<"No cuenta con saldo suficiente para realizar la transaccion";
+				cout<<"No cuenta con saldo suficiente para realizar la transaccion"<<endl;
 			}
 			else{
 				saldo = saldo_inicial - retiro;
-				cout<<"Su nuevo saldo es de: "<<saldo;
+				cout<<"Su nuevo saldo es de: "<<saldo<<endl;break;
 			}
 			
 		case 3: 
-			cout<<"Digite la cantidad de dinero que desea ingresar: "<<extra;
+			cout<<"Digite la cantidad de dinero que desea ingresar: "<<extra<<endl;
 			cin>>extra;
 			saldo = saldo_inicial + extra;
 			cout<<"Su nuevo saldo es de: "<<saldo;break;
@@ -70,6 +78,8 @@ int main(){
 			
 		case 5: break;
 	}	
-
+	
+	system("pause");
+	getch();
 	return 0;
-
+}
