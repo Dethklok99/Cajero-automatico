7873fd1b7306104e283949a39ebc54943c36a24b
Dethklok99 <77636853+Dethklok99@users.noreply.github.com>
2021-01-26 22:58:41 -0600

    Update Cajero automatico.cpp

--- Cajero automatico.cpp
+++ Cajero automatico.cpp
@@ -1,19 +1,16 @@
-//Cajero automatico//
-
-#include<iostream>
 #include<conio.h>
 #include<stdlib.h>
 
 using namespace std;
 
 int main(){
-	int usuario,user[3] = {300,301,302},PIN[3] = {1357,2468,9182},opc,a;
+	int usuario,user[3] = {300,301,302},PIN[3] = {1357,2468,9182},opc,a,salir;
 	int saldo_inicial = 10000,i,s,pines;
 	double saldo = 0;
 	int retiro,impri;
 	char extra;
 	
-	cout<<"\tBienvenido al Cajero Automatico\n"<<endl;
+	cout<<"\t*** Bienvenido al Cajero Automatico ***\n"<<endl;
 	
 	cout<<"Ingresa tu usuario: "<<endl;
 	cin>>usuario;
@@ -39,31 +36,35 @@ int main(){
 		}
 	}
 		do{
-			conexion:
 			system ("cls");
-			cout<<"\tBienvenido a las opciones del cajero"<<endl;
+			cout<<"\t*** Bienvenido a las opciones del cajero ***"<<endl;
 			cout<<"Elija una opcion:"<<endl;
 			cout<<"1.- Consultar saldo"<<endl;
 			cout<<"2.- Retirar efectivo"<<endl;
 			cout<<"3.- Cargar saldo"<<endl;
 			cout<<"4.- Imprimir saldo"<<endl;
-			cout<<"5.- Salir"<<endl;
+			cout<<"5.- Salir\n"<<endl;
 			cin>>opc;
 	
-			switch(opc){
+		switch(opc){
+			getch();
 			case 1:
 				cout<<"Su saldo es de: "<<saldo_inicial<<endl;
 				cin>>saldo_inicial;
+				break;
+				
 			case 2:
 				cout<<"Digite la cantidad de dinero que desea retirar: "<<endl;
 				cin>>retiro;
-			
+				
 				if(retiro > saldo_inicial){
 					cout<<"No cuenta con saldo suficiente para realizar la transaccion"<<endl;
+				
 				}
 				else{
 					saldo = saldo_inicial - retiro;
-					cout<<"Su nuevo saldo es de: "<<saldo<<endl;				
+					cout<<"Su nuevo saldo es de: "<<saldo<<endl;
+					break;			
 				}	
 							
 			case 3: 
@@ -71,16 +72,20 @@ int main(){
 				cin>>extra;
 				saldo = saldo_inicial + extra;
 				cout<<"Su nuevo saldo es de: "<<saldo;
+				break;
 			
 			case 4:
 				cout<<"Imprimiendo saldo: "<<impri;
 				cin>>impri;
 				impri = saldo_inicial;
 				cout<<"Su saldo impreso es de: "<<saldo;
-			
-			case 5: continue;	
-		
+				break;
+				
+			case 5: 
+				cout<<"Pulse [enter] para salir"<<endl;
+				cin>>salir;
 			}
+			getch();
 		}while(opc != 5);
 	
 	system("pause");
