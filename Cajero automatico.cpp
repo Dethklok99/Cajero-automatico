110a382ac0600b72429b3aadb841e5d350d4c7d5
Dethklok99 <77636853+Dethklok99@users.noreply.github.com>
2021-02-10 22:34:54 -0600

    Update Cajero automatico.cpp

--- Cajero automatico.cpp
+++ Cajero automatico.cpp
@@ -1,4 +1,6 @@
-//Cajero automatico//
+/*Cajero automatico
+Programa desarrollado por Adrian Alains de la Cruz Ventura 1° A Modo Sabatino*/
+
 
 #include<iostream>
 #include<conio.h>
@@ -12,66 +14,62 @@ void Retiro();
 void Cargar();
 void Recibo();
 void Salir();
-
+//Definimos las variables
 float saldo_inicial = 10000, retiro, saldo, cargar;
-
+	
+	
 int main(){
-	int user[3] = {3000,3001,3002},PIN[3] = {1357,2468,9182},opc,a,salir;
+	int user[3] = {3000,3001,3002},PIN[3] = {1357,2468,9182},i,opc,a,salir;
 	float s,pines,usuario;
-	int impri,i;
-	
-	cout<<"\t*** Bienvenido al Cajero Automatico ***\n"<<endl;
-	
-	cout<<"Ingresa tu usuario: "<<endl;
-	cin>>usuario;
+
+//Pantalla de bienvenida                                                                                        
+	cout<<"\t---------------------------------------\n\t-   Bienvenido al Cajero Automatico   -              Xalapa, Veracruz. #QuedateEnCasa\n\t---------------------------------------\n"<<endl; 
+//Usuarios y pines	
+	cout<<"\t\tIngresa tu usuario: ";cin>>usuario;
 	for(i = 0; i < 3; i++){
 		if(usuario == user[i]){
 			a = i;
 		}	
-		
 	}
 	
-	cout<<"Ingresa tu PIN:"<<endl;
-	cin>>pines;
-	if((usuario == user[0] && pines == PIN[0]) ||
-	(usuario == user[1] && pines == PIN[1]) ||
-	(usuario == user[2] && pines == PIN[2])){
+	cout<<"\n\t\tIngresa tu PIN    : ";cin>>pines;
+		if((pines == PIN[i]));
 	
-		
+	   else{
+		cout << "Error de usuario y/o PIN"<<endl;
+		}
+	
+//Opociones del cajero automatico		
 		do{
 			system ("cls");
-			cout<<"\t*** Bienvenido a las opciones del cajero ***"<<endl;
-			cout<<"Elija una opcion:"<<endl;
-			cout<<"1.- Consultar saldo"<<endl;
-			cout<<"2.- Retirar efectivo"<<endl;
-			cout<<"3.- Cargar saldo"<<endl;
-			cout<<"4.- Imprimir saldo"<<endl;
-			cout<<"5.- Salir\n"<<endl;
+			cout<<"\t-------------------------------------------------------\n\t-   Bienvenido a las opciones del cajero automatico   -             No acepte ayuda de desconocidos\n\t-------------------------------------------------------\n"<<endl;
+			cout<<"\t\tElija una opcion:\n"<<endl;
+			cout<<"\t\t1.- Consultar saldo"<<endl;
+			cout<<"\t\t2.- Retirar efectivo"<<endl;
+			cout<<"\t\t3.- Cargar saldo"<<endl;
+			cout<<"\t\t4.- Imprimir saldo"<<endl;
+			cout<<"\t\t5.- Salir\n"<<endl;
+			cout<<"\t\tDigite la opcion que desea realizar: ";
 			cin>>opc;
 	
 		switch(opc){
 			case 1:
 				Saldo();
-	  			getch ();
 				break;
 				
 			case 2:
 				Retiro();
-	  			getch ();
 				break;
 							
 			case 3: 
 				Cargar();
-	 			getch ();
 				break;		
 			case 4:
 				Recibo();
-	  			getch ();
 				break;
 				
 			case 5:
 				Salir();
-				getch();
 				break;
 			
 			default: 
@@ -81,11 +79,7 @@ int main(){
 				}
 			getch();
 			}while(opc != 5);
-			
-		}else{
-				cout << "Error de usuario y/o PIN"<<endl;
-			}	
-	
+				
 	system("pause");
 	getch();
 	return 0;	
@@ -93,51 +87,56 @@ int main(){
 
 void Saldo(){
 	system("cls");
-	cout<<"\n\t**Menu de saldo**"<<endl;
-	cout<<"\n\tSu saldo actual es de " << saldo_inicial <<" pesos."<< endl;
+	cout<<"\n\t-------------------------\n\t-   Consulta de saldo   -\n\t-------------------------"<<endl;
+	cout<<"\n\tSu saldo actual es de: $"<<saldo_inicial<<" pesos. \n"<<endl;
+	cout<<"\tPresione [ENTER] para regresar al menu principal. ";
 }
 
 void Retiro(){
 		system("cls");
-		cout<<"\n\t**Menu de retiro de efectivo**"<<endl;
-	  cout<< "\n\tIngrese la cantidad que desea retirar: "; cin>>retiro;
+		cout<<"\n\t--------------------------\n\t-   Retiro de efectivo   -\n\t--------------------------"<<endl;
+	  cout<< "\n\tIngrese la cantidad que desea retirar: ";cin>>retiro;
 	  	saldo = saldo_inicial - retiro;
 	  	if (saldo>0){
-	  		cout<<"\n\tSu saldo restante es de "<<saldo<<" pesos."<< endl;
+	  		cout<<"\n\tSu nuevo saldo es de: $"<<saldo + cargar<<" pesos. \n"<<endl;
+	  		cout<<"\tPresione [ENTER] para regresar al menu principal. ";
 	 	 }
 	 	 else{
-	  		cout<<"\n\tNo cuenta con la cantidad sufiente para retirar."<< endl;
+	  		cout<<"\n\tNo cuenta con la cantidad suficiente para realizar la transaccion";
 		  }
 }
 
 void Cargar(){
 		system("cls");
-		cout<<"\n\t**Menu de cargar saldo**"<<endl;
-	  cout<<"\n\tIngrese la cantidad que desea cargar: "; cin>>cargar;
-		saldo = saldo_inicial + cargar;
-	  	cout<<"\n\tSu saldo actual es de "<<saldo<< " pesos."<< endl;
+		cout<<"\n\t--------------------\n\t-   Cargar saldo   -\n\t--------------------"<<endl;
+	  cout<<"\n\tIngrese la cantidad que desea cargar: ";cin>>cargar;
+		saldo = saldo + cargar;
+	  	cout<<"\n\tSu nuevo saldo es de: $"<<saldo<<" pesos. \n"<<endl;
+	  	cout<<"\tPresione [ENTER] para regresar al menu principal. ";
 }
-
+//Impresión del archivo .txt
 void Recibo(){
 	system("cls");
-	ofstream ticket;
+	ofstream recibo;
 	
-	ticket.open("recibo.txt", ios::out);
+	recibo.open("Recibo.txt", ios::out);
 	
-	if(ticket.fail()){
-		cout<<"ERRROR no se puede imprimir el recibo";
+	if(recibo.fail()){
+		cout<<"NO se puede imprimir el recibo. ";
 		exit(1);
 	}
 	
-	cout<<"\tRecibo creado"<<endl;
-	ticket<<"\tRecibo del cajero."<<endl;
-	ticket<<"\t\nSu saldo actual es de "<<cargar + saldo_inicial<<" pesos."<<endl;
-	ticket<<"\t\nGracias por su preferencia."<<endl;
+	cout<<"\n\t-------------------------------\n\t-   Su recibo se ha impreso   -\n\t-------------------------------\n"<<endl;
+	recibo<<"\n\tRecibo generado. \n"<<endl;
+	recibo<<"\tSu saldo actual es de: $"<<saldo<<" pesos. ";
+	cout<<"\tPresione [ENTER] para regresar al menu principal. ";
 	
-	ticket.close();
+	recibo.close();
 }
 
 void Salir(){
 	system("cls"); 
-		cout<< "\n\t\tFue un placer poder atenderle."<<endl;	
+		cout<<"\t--------------------------------------\n\t-   Fue un placer poder atenderle.   -\n\t--------------------------------------\n"<<endl;
+		cout<<"\tPresione [ENTER] para salir. "<<endl;
+		exit(1);	
 }
